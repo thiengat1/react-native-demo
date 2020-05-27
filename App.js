6883adde125ec7087a98f2b8798b14bd6642008d
thienga <thiengat1@gmail.com>
2020-05-26 20:41:01 -0700

    demo-react-native

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import CategoryListItem from "./components/CategoryListItem"
+
+
+export default class App extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <CategoryListItem/>
+        <CategoryListItem/>
+        <CategoryListItem/>
+        <CategoryListItem/>
+        <CategoryListItem/>
+      </View>
+    );
+  }
+ 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems:'stretch',
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft:16,
+    paddingRight:16
   },
 });
---
+++ components/CategoryListItem.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+    View,
+    Image,
+    Text,
+    StyleSheet
+} from 'react-native';
+import Guitar from '../assets/guitar.png';
+
+export default function CategoryListItem(props){
+    return (
+        <View style={style.container}>
+            <Text style={style.title}>CategoryListItem</Text>
+            <Image style={style.categoryImage} source={Guitar}/>
+        </View>
+    )
+}
+const style= StyleSheet.create({
+    container:{
+        alignItems:"center",
+        padding:16,
+        borderRadius:4,
+        backgroundColor:'#FFF',
+        shadowColor:'#000',
+        shadowOpacity:0.1,
+        shadowRadius:10,
+        shadowOffset:{width:0,height:0},
+        marginBottom:16
+
+    },
+    categoryImage:{
+        width:64,
+        height:64
+
+    },
+    title:{
+        textTransform:"uppercase",
+        marginBottom:8,
+        fontWeight:'700'
+    }
+})
